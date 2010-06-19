430e5cabaaeb872c125f76d30d8318124a96152d
Mike Gunderloy <MikeG1@larkfarm.com>
2010-06-19 16:57:14 -0500

    Add recipe for cron management via whenever

---
+++ recipes/cron.rb
@@ -0,0 +1,11 @@
+# If you use whenever to manage cron jobs inside of your Rails app
+namespace :cron do
+  desc "Update the current crontab with the configuration in config/schedule.rb"
+  task :update, :roles => :db, :only => { :primary => true } do
+    rails_env = fetch(:rails_env, "production")
+    run "cd #{release_path} && whenever --update-crontab --set environment=#{rails_env} -i #{application}"
+  end
+end
+# To activate:
+#   after "deploy:symlink", "cron:update"
+
