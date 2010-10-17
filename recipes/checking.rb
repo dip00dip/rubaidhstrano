78984c9e2c1839881c046e810a93329dd0194d1a
Mike Gunderloy <MikeG1@larkfarm.com>
2010-10-17 10:51:57 -0500

    Add deploy:ask_production task

--- recipes/checking.rb
+++ recipes/checking.rb
@@ -14,7 +14,7 @@ namespace :deploy do
 
   desc "Require positive confirmation of deployment to production"
   task :ask_production do
-    ask_prompt = "Are you sure you want to work with production? \nIf so, type in \"yes\" (anything else is a \"no\"): ";
+    ask_prompt = fetch(:production_deployment_prompt, "Are you sure you want to work with production? \nIf so, type in \"yes\" (anything else is a \"no\"): ");
     exit unless Capistrano::CLI.ui.ask(ask_prompt).downcase == 'yes'
   end
 
