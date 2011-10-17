7e24a51603ea5a1296de0167cf2eaa637c056193
Mike Gunderloy <MikeG1@larkfarm.com>
2011-10-17 07:54:01 -0500

    Make sure we can find the whenever binary on Rails 3

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -3,7 +3,11 @@ namespace :cron do
   desc "Update the current crontab with the configuration in config/schedule.rb"
   task :update, :roles => :db, :only => { :primary => true } do
     rails_env = fetch(:rails_env, "production")
-    run "cd #{release_path} && whenever --update-crontab --set environment=#{rails_env} -i #{application}"
+    if defined?(Bundler)
+      run "cd #{release_path} && bundle exec whenever --update-crontab --set environment=#{rails_env} -i #{application}"
+    else
+      run "cd #{release_path} && whenever --update-crontab --set environment=#{rails_env} -i #{application}"
+    end
   end
 end
 # To activate:
