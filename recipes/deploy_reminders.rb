94df9e63a55bf280747879cf9c175b165f7acec6
Mike Gunderloy <MikeG1@larkfarm.com>
2011-11-06 13:14:06 -0600

    Add reminder tasks

---
+++ recipes/deploy_reminders.rb
@@ -0,0 +1,31 @@
+after "deploy", "deploy:reminders:show"
+after "deploy", "deploy:reminders:clear"
+
+namespace :deploy do
+  namespace :reminders do
+    def reminder_file
+     "#{shared_path}/system/reminders.txt"
+    end
+
+    desc "adds a reminder that will be shown on deploy in the console of the next deployer"
+    task :add, :roles => :reminder_host do
+      set :reminder, Capistrano::CLI.ui.ask("Reminder: ")
+      run "echo \"user: #{user}\" >> #{reminder_file}"
+      run "echo \"On: #{Time.new.to_s}\" >> #{reminder_file}"
+      run "echo \"Message: #{reminder}\" >> #{reminder_file}"
+      run "echo \"\n\" >> #{reminder_file}"
+    end
+
+    desc "shows all the current reminder messages"
+    task :show, :roles => :reminder_host do
+      run "if [ -e #{reminder_file} ]; then cat #{reminder_file}; fi"
+    end
+
+    desc "clears the current reminder messages"
+    task :clear, :roles => :reminder_host do
+      run "rm #{reminder_file}"
+    end
+  end
+end
+
+
